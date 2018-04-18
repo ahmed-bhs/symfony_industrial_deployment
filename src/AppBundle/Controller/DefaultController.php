eee58440415e7b4b913d76bb94a6b30f0a4a3811
Gauthier <mysterandco@gmail.com>
2018-04-18 10:06:18 +0200

    Updated

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/api", name="api")
+     */
+    public function apiAction(Request $request)
+    {
+
+    }
 }
